4e7198fc5f65b8e81b581f3d2e2ac7bd59848cbc
Adrian Manuel T. Cleofe <adriancleofe@gmail.com>
2020-04-24 18:45:08 +0800

    created custom hookds

---
+++ hooks/http.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+
+const encodeFormData = (data) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
+
+export const useHttp = (method, data) => {
+  const settings = useSelector((state) => state.settings);
+  const [fetchedData, setFetchedData] = useState(null);
+
+  useEffect(() => {
+    fetch(settings.url + `/webservice.php?${encodeFormData(data)}`, {
+      method: method,
+      headers: {
+        Accept: "'application/json'",
+        "Access-Control-Allow-Origin": "*",
+      },
+    })
+      .then((res) => {
+        try {
+          return res.json();
+        } catch (err) {
+          console.warn(e);
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        setFetchedData(data);
+      });
+  }, [settings]);
+
+  return [fetchedData];
+};
