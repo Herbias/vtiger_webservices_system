c27f8112a1714d4503bb8df64373021465c784c0
Adrian Manuel T. Cleofe <adriancleofe@gmail.com>
2020-04-24 18:42:53 +0800

    added link

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,16 +1,23 @@
+import Link from "next/link";
+
 export default ({ Show }) => {
   return (
     <nav className="p-4 mb-2 shadow rounded bg-white text-gray-600">
       <ul className="flex">
         <li className="mr-6">
-          <a className="text-blue-500 hover:text-blue-800" href="#">
-            Tickets
-          </a>
+          <Link href="/">
+            <a className="text-blue-500 hover:text-blue-800">Home</a>
+          </Link>
         </li>
         <li className="mr-6">
-          <a className="text-blue-500 hover:text-blue-800" href="#">
-            Organization
-          </a>
+          <Link href="/tickets">
+            <a className="text-blue-500 hover:text-blue-800">Tickets</a>
+          </Link>
+        </li>
+        <li className="mr-6">
+          <Link href="/Organization">
+            <a className="text-blue-500 hover:text-blue-800">Organization</a>
+          </Link>
         </li>
         <li className="mr-6">
           <a
