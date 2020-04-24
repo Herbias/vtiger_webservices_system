a3e610f8ac39ec7d682cde8b613d396a05ca943f
Adrian Manuel T. Cleofe <adriancleofe@gmail.com>
2020-04-24 18:43:59 +0800

    update to use http request

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,36 @@
 import MainLayout from "../layout/MainLayout";
 import { useSelector } from "react-redux";
 import { useState, useEffect } from "react";
+import { useHttp } from "../hooks/http";
 
 export default () => {
   const [message, setMessage] = useState({ style: "", str: "" });
   const settings = useSelector((state) => state.settings);
 
+  let getChallenge = useHttp("GET", {
+    operation: "getchallenge",
+    username: settings.user,
+  });
+
+  console.log(getChallenge);
+
   useEffect(() => {
-    settings.url === "" || settings.user === "" || settings.accesKey === ""
-      ? setMessage({
-          style: "text-red-500",
-          str: "It seems web services is not configure correctly",
-        })
-      : setMessage({
-          style: "text-green-500",
-          str: "Welcome to VTiger Web Services",
-        });
+    if (
+      settings.url === "" ||
+      settings.user === "" ||
+      settings.accesKey === "" ||
+      getChallenge.success == false
+    ) {
+      setMessage({
+        style: "text-red-500",
+        str: "It seems web services is not configure correctly",
+      });
+    } else {
+      setMessage({
+        style: "text-green-500",
+        str: "Welcome to VTiger Web Services",
+      });
+    }
   }, [settings]);
 
   return (
